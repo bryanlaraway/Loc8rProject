ae110db4b25f0be4d7ca1bb9aa39c048ed3b72dd
bryanlaraway <bryanlaraway@gmail.com>
2016-06-05 21:34:50 -0700

    API additions.

--- app_api/controllers/reviews.js
+++ app_api/controllers/reviews.js
@@ -7,13 +7,70 @@ var sendJsonResponse = function(res, status, content) {
 };
 
 
+module.exports.locationsListByDistance = function(req, res) {
+    var lng = parseFloat(req.query.lng);
+    var lat = parseFloat(req.query.lat);
+    var point = {
+        type: "Point",
+        coordinates: [lng, lat]
+    };
+    Loc.geoNear(point, options, callback);
+};
+
+
+
+
 module.exports.reviewsCreate = function (req, res) {
     sendJsonResponse(res, 200, {"status" : "success"});
 };
 
 module.exports.reviewsReadOne = function (req, res) {
-    sendJsonResponse(res, 200, {"status" : "success"});
+    if (req.params && req.params.locationid && req.params.reviewid) {
+        Loc.findById(req.params.locationid)
+            .select('name reviews')
+            .exec(function (err, location) {
+                    var response, review;
+                    if (!location) {
+                        sendJsonResponse(res, 404, {
+                            "message": "locationid not found"
+                        });
+                        return;
+                    } else if (err) {
+                        sendJsonResponse(res, 400, err);
+                        return;
+                    }
+                    if (location.reviews && location.reviews.length > 0) {
+                        review = location.reviews.id(req.params.reviewid);
+                        if (!review) {
+                            sendJsonResponse(res, 404, {
+                                "message": "reviewid not found"
+                            });
+                        } else {
+                            response = {
+                                location: {
+                                    name: location.name,
+                                    id: req.params.locationid
+                                },
+                                review: review
+                            };
+                            sendJsonResponse(res, 200, response);
+                        }
+                    } else {
+                        sendJsonResponse(res, 404, {
+                            "message": "No reviews found"
+                        });
+                    }
+                }
+            );
+    } else {
+    sendJsonResponse(res, 404, {
+        "message": "Not found, locationid and reviewid are both required"
+    });
+    }
 };
+
+
+
 
 module.exports.reviewsUpdateOne = function (req, res) {
     sendJsonResponse(res, 200, {"status" : "success"});
